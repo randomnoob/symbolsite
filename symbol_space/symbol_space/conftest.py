964ad81b25193770104baca348d3b18ea773d1e1
pippy@coquan <pippy@coquan.com>
2021-07-08 10:39:49 +0700

    replace django default with cookiecutter django layout

---
+++ symbol_space/symbol_space/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from symbol_space.users.models import User
+from symbol_space.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
